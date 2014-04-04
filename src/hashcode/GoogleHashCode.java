15901d0fec67227ecf6764c3690b3c3d79bb8b06
Cristian Saita <acatias@gmail.com>
2014-04-04 22:33:33 +0200

    v2

--- src/hashcode/GoogleHashCode.java
+++ src/hashcode/GoogleHashCode.java
@@ -9,9 +9,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class GoogleHashCode {
-
-	public static void main(String[] args) {
-				
+	
+	public static void bete() {
+		
+		
 		try {
 			FileReader fr = new FileReader(new File(
 									"C:/github/GoogleHashCode/doodle.txt"));
@@ -38,7 +39,7 @@ public class GoogleHashCode {
 				int colNum = Integer.valueOf(columns);
 						
 				System.out.println(rowNum + "$" + colNum + "$");
-
+		
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -109,7 +110,7 @@ public class GoogleHashCode {
 				int colNum1 = Integer.valueOf(columns1);
 						
 				System.out.println(rowNum1 + "$" + colNum1 + "$");
-
+		
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -156,6 +157,148 @@ public class GoogleHashCode {
 			}
 			
 			bw1.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+	}
+	
+	public static void eraseMatrix(int[][] matrix, LargestSquare.Square sq) {
+		
+		for(int i = sq.startLine; i < sq.startLine - sq.size; i-- ) {
+			
+			for(int j = sq.startColumn; j < sq.startColumn - sq.size; j-- ) {
+				
+				matrix[i][j] = 0;				
+			}
+		}
+		
+	}
+	
+	
+
+	public static void main(String[] args) {
+		
+		try {
+			FileReader fr = new FileReader(new File(
+									"C:/github/GoogleHashCode/doodle.txt"));
+			
+			FileWriter fw = new FileWriter(new File ("C:/github/GoogleHashCode/out2.txt"));
+						
+			BufferedReader br = new BufferedReader(fr);
+			
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			String line = "";
+
+			int rowNum = 0;
+			
+			int colNum = 0;
+
+			try {
+				line = br.readLine();
+				
+				String rows = line.substring(0, line.indexOf(" "));
+				String columns = line.substring(line.indexOf(" ") + 1);
+				
+				System.out.println(rows + "$" + columns + "$");
+				
+				rowNum = Integer.valueOf(rows);
+				
+				colNum = Integer.valueOf(columns);
+						
+				System.out.println(rowNum + "$" + colNum + "$");
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			int[][] matrix = new int[rowNum][colNum];
+			
+			long numComs = 0;
+			rowNum = 0;
+			
+			while(line != null) {
+				
+				try {
+					
+					line = br.readLine();
+					
+					if (line == null) {
+						break;
+					}
+					
+					for (int i = 0; i < line.length(); i++) {
+						
+						if (line.charAt(i) == '#') {
+							
+//							bw.write("PAINTSQ " + rowNum + " " + i + " 0");
+//							bw.write("\n");
+							
+							numComs++;
+							
+							matrix[rowNum][i] = 1;
+							
+						} else {
+							
+							matrix[rowNum][i] = 0;
+						}
+					}
+					
+					rowNum++;
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			}
+			
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			
+			StringBuilder sb = new StringBuilder();
+			
+			int numInst = 0;
+			
+			while (LargestSquare.getMaxSquare(matrix) != null) {
+				
+				LargestSquare.Square sq = LargestSquare.getMaxSquare(matrix);
+				
+				//System.out.println("PAINTSQ " + sq.middleLine + " " + sq.middleColumn + " " + sq.size + "\n");
+				
+				sb.append("PAINTSQ " + sq.middleLine + " " + sq.middleColumn + " " + sq.size + "\n");
+				
+				//System.out.print(sq.startLine + "%" + sq.startColumn + "$" + sq.size);
+				
+				for(int i = sq.startLine; i > sq.startLine - sq.size; i-- ) {
+					
+					
+					
+					for(int j = sq.startColumn; j > sq.startColumn - sq.size; j-- ) {
+						
+						//System.out.println("here");
+						
+						matrix[i][j] = 0;				
+					}
+				}
+				
+				numInst++;
+			}
+			
+			bw.write(numInst + "\n");
+			
+			bw.write(sb.toString());
+			
+			// write the String builder
+			
+			bw.close();
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
