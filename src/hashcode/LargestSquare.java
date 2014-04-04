bc06fdec56d1442f2cb2e53c031e28621fb50467
Cristian Saita <acatias@gmail.com>
2014-04-05 00:34:24 +0200

    Trial Round

--- src/hashcode/LargestSquare.java
+++ src/hashcode/LargestSquare.java
@@ -1,15 +1,10 @@
 package hashcode;
 
-
 public class LargestSquare {
 
-	
+/**
 
-	
-
-	/**
-
-	 * 1) Construct a sum matrix S[R][C] for the given M[R][C].
+1) Construct a sum matrix S[R][C] for the given M[R][C].
 
      a)	Copy first row and first columns as it is from M[][] to S[][]
 
@@ -19,34 +14,23 @@ public class LargestSquare {
 
             S[i][j] = min(S[i][j-1], S[i-1][j], S[i-1][j-1]) + 1
 
-         Else /*If M[i][j] is 0
+         Else If M[i][j] is 0
 
             S[i][j] = 0
 
 2) Find the maximum entry in S[R][C]
 
-3) Using the value and coordinates of maximum entry in S[i], print 
+3) Using the value and coordinates of maximum entry in S[i], 
 
-   sub-matrix of M[][]
+print sub-matrix of M[][]
 
-	 */
-
-
-
+*/
 
 	public static void main(String[] args) {
 
-		
-
 		System.out.println("Hello, World!");
-
 		
-
-		System.out.println("Hello, World!");
-
 	}
-
-	
 
 	public static Square getMaxSquare(int[][] M){
 
@@ -56,104 +40,78 @@ public class LargestSquare {
 
 		int[][] S = new int[numLines][numColumns];
 
-		
-
 		/* Set first column of S[][]*/
 
-		  for(int i = 0; i < numLines; i++){
+		for(int i = 0; i < numLines; i++){
+			
+			S[i][0] = M[i][0];
 
-		     S[i][0] = M[i][0];
+		}
 
-		  }
+		/* Set first row of S[][]*/    
+		
+		for(int j = 0; j < numColumns; j++){
 
-		  
+			S[0][j] = M[0][j];
 
-		  /* Set first row of S[][]*/    
+		}
 
-		  for(int j = 0; j < numColumns; j++){
+		/* Construct other entries of S[][]*/
 
-		     S[0][j] = M[0][j];
+		for(int i = 1; i < numLines; i++){
 
-		  }
+			for(int j = 1; j < numColumns; j++){
 
-		      
+				if(M[i][j] == 1) { 
 
-		  /* Construct other entries of S[][]*/
+					S[i][j] = Math.min(S[i][j-1], Math.min(S[i-1][j], S[i-1][j-1])) + 1;
+		        
+				} else {
 
-		  for(int i = 1; i < numLines; i++){
+					S[i][j] = 0;
+				}
+			}    
+		}
 
-		    for(int j = 1; j < numColumns; j++){
+		int maxSquareSize = 0;
 
-		      if(M[i][j] == 1) 
+		Square maxSquare = null;
 
-		        S[i][j] = Math.min(S[i][j-1], Math.min(S[i-1][j], S[i-1][j-1])) + 1;
+		for(int i = 0; i < numLines; i++){
 
-		      else
+			for(int j = 0; j < numColumns; j++){
 
-		        S[i][j] = 0;
-
-		    }    
-
-		  }
-
-		  
-
-		  int maxSquareSize = 0;
-
-		  Square maxSquare = null;
-
-		  for(int i = 0; i < numLines; i++){
-
-			    for(int j = 0; j < numColumns; j++){
-
-			    	int squareSize = S[i][j];
-
-
+				int squareSize = S[i][j];
 			    	
-if(squareSize!=0){
+				if (squareSize != 0) {
 
-			    		if(squareSize >maxSquareSize && (squareSize%2!=0)){
+					if (squareSize > maxSquareSize && (squareSize % 2 != 0)) {
 
-			    			maxSquare = new Square(i, j,squareSize);
+						maxSquare = new Square(i, j, squareSize);
 
-			    			maxSquareSize = squareSize;
+						maxSquareSize = squareSize;
 
-			    		}else{
+					} else {
 
-			    			if(squareSize >maxSquareSize && (squareSize%2!=1)){
+						if(squareSize > maxSquareSize && (squareSize % 2 != 1)) {
 
-				    			maxSquare = new Square(i, j,squareSize-1);
-
-				    			maxSquareSize = squareSize-1;
-
-
-			    			}
-
-			    		}
-
-			    	}
-
-			 }    
-
-		  } 
+							maxSquare = new Square(i, j, squareSize - 1);
+							
+							maxSquareSize = squareSize-1;
+						}
+					}
+				}
+			}    
+		} 
 
 		return maxSquare;
-
 	}
 
-	
-
-	
-
-	public static class Square{
+	public static class Square {
 
 		int middleLine, middleColumn;
 
-		int startLine,startColumn;
-
-
-
-
+		int startLine, startColumn;
 
 		int size;
 
@@ -163,20 +121,13 @@ if(squareSize!=0){
 
 			this.size = size;
 
-			this.middleLine = startLine - size +1 +size/2;
+			this.middleLine = startLine - size + 1 + size / 2;
 
-			this.middleColumn = startColumn  - size +1 +size/2;
-
+			this.middleColumn = startColumn - size + 1 + size/2;
 
 			this.startLine = startLine;
 
 			this.startColumn = startColumn;
-
 		}
-
 	}
-
-
-
-
 }
---
+++ src/hashcode/TrialRound.java
@@ -0,0 +1,283 @@
+package hashcode;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TrialRound {
+
+	public static void trialV1() {
+		
+		try {
+			FileReader fr = new FileReader(new File("C:/github/GoogleHashCode/doodle.txt"));
+			
+			FileWriter fw = new FileWriter(new File ("C:/github/GoogleHashCode/out.txt"));
+						
+			BufferedReader br = new BufferedReader(fr);
+			
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			String line = "";
+			
+			try {
+				line = br.readLine();
+				
+				String rows = line.substring(0, line.indexOf(" "));
+				String columns = line.substring(line.indexOf(" ") + 1);
+				
+				System.out.println(rows + "$" + columns + "$");
+				
+				int rowNum = Integer.valueOf(rows);
+				
+				int colNum = Integer.valueOf(columns);
+						
+				System.out.println(rowNum + "$" + colNum + "$");
+		
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			long numComs = 0;
+			long rowNum = 0;
+			
+			while(line != null) {
+				
+				try {
+					
+					line = br.readLine();
+					
+					if (line == null) {
+						break;
+					}
+					
+					for (int i = 0; i < line.length(); i++) {
+						
+						if (line.charAt(i) == '#') {
+							
+							bw.write("PAINTSQ " + rowNum + " " + i + " 0");
+							bw.write("\n");
+							
+							numComs++;
+							
+						}
+					}
+					
+					rowNum++;
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			}
+			
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			bw.close();
+			
+			FileReader fr1 = new FileReader(new File(
+									"C:/github/GoogleHashCode/doodle.txt"));
+			
+			FileWriter fw1 = new FileWriter(new File ("C:/github/GoogleHashCode/out1.txt"));
+						
+			BufferedReader br1 = new BufferedReader(fr1);
+			
+			BufferedWriter bw1 = new BufferedWriter(fw1);
+			
+			String line1 = "";
+			
+			try {
+				line1 = br1.readLine();
+				
+				String rows1 = line1.substring(0, line1.indexOf(" "));
+				String columns1 = line1.substring(line1.indexOf(" ") + 1);
+				
+				System.out.println(rows1 + "$" + columns1 + "$");
+				
+				int rowNum1 = Integer.valueOf(rows1);
+				
+				int colNum1 = Integer.valueOf(columns1);
+						
+				System.out.println(rowNum1 + "$" + colNum1 + "$");
+		
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			long rowNum1 = 0;
+			
+			bw1.write(numComs + "\n");
+			
+			while(line1 != null) {
+				
+				try {
+					
+					line1 = br1.readLine();
+					
+					if (line1 == null) {
+						break;
+					}
+					
+					for (int i = 0; i < line1.length(); i++) {
+						
+						if (line1.charAt(i) == '#') {
+							
+							bw1.write("PAINTSQ " + rowNum1 + " " + i + " 0");
+							bw1.write("\n");
+														
+						}
+					}
+					
+					rowNum1++;
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			}
+			
+			try {
+				br1.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			bw1.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+	}
+	
+	public static void eraseMatrix(int[][] matrix, LargestSquare.Square sq) {
+		
+		for(int i = sq.startLine; i < sq.startLine - sq.size; i-- ) {
+			
+			for(int j = sq.startColumn; j < sq.startColumn - sq.size; j-- ) {
+				
+				matrix[i][j] = 0;				
+			}
+		}
+	}
+
+	public static void trialV2() {
+		
+		try {
+			FileReader fr = new FileReader(new File("C:/github/GoogleHashCode/doodle.txt"));
+			
+			FileWriter fw = new FileWriter(new File ("C:/github/GoogleHashCode/out2.txt"));
+						
+			BufferedReader br = new BufferedReader(fr);
+			
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			String line = "";
+
+			int rowNum = 0;
+			
+			int colNum = 0;
+
+			try {
+				line = br.readLine();
+				
+				String rows = line.substring(0, line.indexOf(" "));
+				String columns = line.substring(line.indexOf(" ") + 1);
+				
+				System.out.println(rows + "$" + columns + "$");
+				
+				rowNum = Integer.valueOf(rows);
+				
+				colNum = Integer.valueOf(columns);
+						
+				System.out.println(rowNum + "$" + colNum + "$");
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			int[][] matrix = new int[rowNum][colNum];
+			
+			rowNum = 0;
+			
+			while(line != null) {
+				
+				try {
+					
+					line = br.readLine();
+					
+					if (line == null) {
+						break;
+					}
+					
+					for (int i = 0; i < line.length(); i++) {
+						
+						if (line.charAt(i) == '#') {
+																					
+							matrix[rowNum][i] = 1;
+							
+						} else {
+							
+							matrix[rowNum][i] = 0;
+						}
+					}
+					
+					rowNum++;
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			}
+			
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			StringBuilder sb = new StringBuilder();
+			
+			int numInst = 0;
+			
+			while (LargestSquare.getMaxSquare(matrix) != null) {
+				
+				LargestSquare.Square sq = LargestSquare.getMaxSquare(matrix);
+				
+				sb.append("PAINTSQ " + sq.middleLine + " " + sq.middleColumn + " " + (sq.size - 1) / 2 + "\n");
+				
+				for(int i = sq.startLine; i > sq.startLine - sq.size; i-- ) {
+
+					for(int j = sq.startColumn; j > sq.startColumn - sq.size; j-- ) {
+						
+						matrix[i][j] = 0;				
+					}
+				}
+				
+				numInst++;
+			}
+			
+			bw.write(numInst + "\n");
+			
+			bw.write(sb.toString());
+			
+			bw.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+	}
+
+}
