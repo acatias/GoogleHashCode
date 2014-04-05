323b6b46787a2fca885a9f03b1cb132276098d6f
Guy TONYE <guy@smartsy.us>
2014-04-05 16:24:20 +0200

    update prim algorithm

--- src/tonye/Car.java
+++ src/tonye/Car.java
@@ -6,10 +6,14 @@ public class Car {
 
 	public int carIndex;
 	public int currentNodeIndex;
+	public int availableTime;
+	public boolean canMove;
 	public ArrayList<Integer> visitedNodes;
 	
 	public Car(int carIndex){
 		this.carIndex = carIndex;
+		this.visitedNodes = new ArrayList<Integer>();
+		this.canMove= true; 
 	}
 	
 	public void addNode(int nodeIndex){
--- src/tonye/MyPrimAlgorithm.java
+++ src/tonye/MyPrimAlgorithm.java
@@ -27,13 +27,14 @@ public class MyPrimAlgorithm {
 	}
 	
 	public static boolean hasUnmarkedNeighbour(Node[] junctions, ArrayList<Node> markedJunctions){
-		boolean found = false;
-		for (int i = 0; (!found) && (i < junctions.length); i++){
-			for (int j = 0; (!found) && (j < markedJunctions.size()); j++){
-				found = (i == markedJunctions.get(j).index);
+		boolean res = true;
+		for (int j = 0; (res) && (j < markedJunctions.size()); j++){
+			for (Route road : markedJunctions.get(j).routes){
+				if (isAMarkedJunction(getDestinationNode(markedJunctions.get(j).index, road), markedJunctions))
+					res = false;
 			}
 		}
-		return (found);
+		return (res);
 	}
 	
 	/**
@@ -46,12 +47,15 @@ public class MyPrimAlgorithm {
 	public static Car[] initializeCars(int nbCars, int nodeIndex){
 		Car[] cars = new Car[nbCars];
 		for (int i = 0; i < nbCars; i++){
-			cars[i].carIndex = i;
-			cars[i].visitedNodes.add(nodeIndex);
+			cars[i] = new Car(i);
+			cars[i].addNode(nodeIndex);
 		}
-		return null;
+		return cars;
 	}
 	
+	/**
+	 * @return true if there is a car on the specified node
+	 * **/
 	public static boolean hasACarAvailable(int nodeIndex, Car[] cars){
 		boolean found = false;
 		for (int i = 0; (!found) && (i < cars.length); i++){
@@ -61,19 +65,29 @@ public class MyPrimAlgorithm {
 		}
 		return (found);
 	}
-	
+
+	/**
+	 * @return Reachable the chosen junction given the available junctions and the time
+	 * **/
 	public static Reachable getBestJunction(ArrayList<Reachable> reacheablesJunctions, int timelimit){
 		Reachable best = null;
+
 		for (Reachable reachable : reacheablesJunctions){
+
 			if (reachable.road.time <= timelimit){
-				if ((reachable.road.dist / reachable.road.time) < (best.road.dist / best.road.time)){
+				if (best == null){
+					best = reachable;
+				} else if ((reachable.road.dist / reachable.road.time) < (best.road.dist / best.road.time)){
 					best = reachable;
 				}
 			}
 		}
 		return (best);
 	}
-	
+
+	/**
+	 * this method will pick a car and moves it to a node
+	 * **/
 	public static void moveCar(Car[] cars, Reachable best){
 		int startNodeIndex = getDestinationNode(best.node.index, best.road);
 		Car car = null;
@@ -89,7 +103,7 @@ public class MyPrimAlgorithm {
 	 * @param Node[] all the junctions
 	 * this method will build a minimum tree coverage for the junctions
 	 * **/
-	public static int MyPrimAlgorithm(Node[] junctions){
+	public static int computeMyPrimAlgorithm(Node[] junctions){
 		//EXTRACT THE STARTING NODE
 		Node startJunction;
 		int availableTime;
@@ -107,22 +121,37 @@ public class MyPrimAlgorithm {
 		//BUILD THE MARKED NODES ARRAY
 		ArrayList<Node> markedJunctions = new ArrayList <Node> ();
 		markedJunctions.add(startJunction);
-		
+
 		//LOOP WHILE THE TREE CAN GROW
-		while (!hasUnmarkedNeighbour(junctions, markedJunctions)){
-			
+		while (hasUnmarkedNeighbour(junctions, markedJunctions)){
+
 			//BUILD A LIST OF JUNCTIONS THAT CAN BE JOINED 
 			ArrayList<Reachable> reacheablesJunctions = new ArrayList<Reachable>();
+
+			for (int i = 0; (cars != null) && (i < cars.length); i++){
+				for (int j = 0; (j < junctions[cars[i].currentNodeIndex].routes.size()); j++){
+					Route road = markedJunctions.get(i).routes.get(j);
+					int destNodeId = getDestinationNode(markedJunctions.get(i).index, road);
+
+					//IF THE ROAD HAS NOT BEEN VISITED OR THE REACHABLE JUNCTION IS NOT MARKED ADD THE JUNCTIONS
+					if ((!road.isVisited)){
+						reacheablesJunctions.add(new Reachable(junctions[destNodeId], road));
+					}
+				}
+			}
 			
 			//LOOP ON MARKED ARRAY TO GET ALL REACHABLE JUNCTIONS
 			for (int i = 0; i < markedJunctions.size(); i++){
-				if (!hasACarAvailable(markedJunctions.get(i).index, cars))
+				if (!hasACarAvailable(markedJunctions.get(i).index, cars)){
+					System.out.println("out for anavailable car");
 					continue;
+				}
 				//CHECK EACH MARKED JUNCTION ROADS TO GET THE REACHABLE NODES
 				for (int j = 0; (j < markedJunctions.get(i).routes.size()); j++){
+
 					Route road = markedJunctions.get(i).routes.get(j);
 					int destNodeId = getDestinationNode(markedJunctions.get(i).index, road);
-					
+
 					//IF THE ROAD HAS NOT BEEN VISITED OR THE REACHABLE JUNCTION IS NOT MARKED ADD THE JUNCTIONS
 					if ((!road.isVisited) && !isAMarkedJunction(destNodeId, markedJunctions)){
 						reacheablesJunctions.add(new Reachable(junctions[destNodeId], road));
@@ -130,13 +159,23 @@ public class MyPrimAlgorithm {
 				}
 			}
 		
-			//CHECK ON ALL THE REACHABLES THE ONE THE ONE WITH THE HIGHT RATIO DISTANCE TIME
+			//CHECK ON ALL THE REACHABLES THE ONE THE ONE WITH THE HIGHT RATIO DISTANCE TIM
+			System.out.println("reachable junction size " + reacheablesJunctions.size());
 			Reachable best = getBestJunction(reacheablesJunctions, availableTime);
-			if (best == null)
+			if (best == null){
+				System.out.println("out for no best junction found");
 				break;
+			}
 			availableTime -= best.road.time;
 			best.road.isVisited = true;
 			moveCar(cars, best);
+			markedJunctions.add(best.node);
+		}
+		for (int i = 0; (cars != null) && (i < cars.length); i++){
+			System.out.println(cars[i].visitedNodes.size());
+			for (int j = 0; j < cars[i].visitedNodes.size(); j++){
+				System.out.println(cars[i].visitedNodes.get(j));
+			}
 		}
 		return (0);
 	}
